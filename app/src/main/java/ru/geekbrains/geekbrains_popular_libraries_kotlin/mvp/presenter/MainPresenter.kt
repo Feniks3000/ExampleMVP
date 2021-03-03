60897afe10232d770c134bab8d6f8654db2bb068
Trofimov <iet@kalite.ro>
2021-03-03 08:26:18 +0300

    Объединил кнопки в список и передаю в презентер порядковый номер кнопки в списке.

--- app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/presenter/MainPresenter.kt
@@ -1,26 +1,16 @@
 package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter
 
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.R
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.CountersModel
 import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.MainView
 
 class MainPresenter(val mainView: MainView) {
-    val model = CountersModel()
+    val model = CountersModel
 
-    fun counterClick(id: Int){
-        when(id){
-            R.id.btn_counter1 -> {
-                val nextValue = model.next(0)
-                mainView.setButtonText(0, nextValue.toString())
-            }
-            R.id.btn_counter2 -> {
-                val nextValue = model.next(1)
-                mainView.setButtonText(1, nextValue.toString())
-            }
-            R.id.btn_counter3 -> {
-                val nextValue = model.next(2)
-                mainView.setButtonText(2, nextValue.toString())
-            }
-        }
+    fun counterClick(id: Int) {
+        mainView.setButtonText(id, model.getNext(id).toString())
+    }
+
+    fun initButtonText(id: Int) {
+        mainView.setButtonText(id, model.getCurrent(id).toString())
     }
 }
