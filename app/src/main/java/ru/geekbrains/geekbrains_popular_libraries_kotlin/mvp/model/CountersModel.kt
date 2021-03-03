19950c3e8a835422ca47654dfe23d0e942cb7c3b
Trofimov <iet@kalite.ro>
2021-03-03 08:27:48 +0300

    Сделал модель синглтоном, убрал инициализацию счетчиков в макете и добавил обновление значений при создании активити.

--- app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/model/CountersModel.kt
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/model/CountersModel.kt
@@ -1,12 +1,11 @@
 package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model
 
-class CountersModel {
-
+object CountersModel {
     val counters = mutableListOf(0, 0, 0)
 
     fun getCurrent(index: Int) = counters[index]
 
-    fun next(index: Int): Int {
+    fun getNext(index: Int): Int {
         counters[index]++
         return getCurrent(index)
     }
