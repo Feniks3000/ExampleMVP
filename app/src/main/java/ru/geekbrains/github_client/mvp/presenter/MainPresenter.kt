0f1ccaaf103322e5d0c6e45245592c02d43f49eb
Trofimov <iet@kalite.ro>
2021-03-19 05:47:13 +0300

    Добавил библиотеки Rx и подтянул примеры с урока

--- app/src/main/java/ru/geekbrains/github_client/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/ru/geekbrains/github_client/mvp/presenter/MainPresenter.kt
@@ -4,12 +4,16 @@ import com.github.terrakok.cicerone.Router
 import moxy.MvpPresenter
 import ru.geekbrains.github_client.mvp.navigation.IScreens
 import ru.geekbrains.github_client.mvp.view.MainView
+import ru.geekbrains.github_client.rx_learn.Operators
 
 class MainPresenter(val router: Router, val screens: IScreens) : MvpPresenter<MainView>() {
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         router.replaceScreen(screens.users())
+
+        //Creation().exec()
+        Operators().exec()
     }
 
     fun backClicked() {
